09da48b4b88f455a96c920c31ef2b54392e3851c
ganeshneo46 <ardalkarg@gmail.com>
2022-11-30 22:20:26 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const mobile_nav = document.querySelector(".mobile-navbar-icon");
+
+const nav_header = document.querySelector('.navbar');
+
+
+const toggleNavbar = () => {
+    nav_header.classList.toggle("active");
+}
+
+mobile_nav.addEventListener('click', ()=> toggleNavbar())
